477b8485abc6f319ba8c76129b7d5533d1ffa3f6
Latifat Oduntan <79084252+latifa-12@users.noreply.github.com>
2021-10-13 16:09:56 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <section className="section about-section">
+     <h1 className="section-title">about us</h1>
+     <p>
+       we have the best cocktails in the world.
+     </p>
+   </section>
+  )
+}
+
+export default About
